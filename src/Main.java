fa8568ba26f5744fff045827e73bc2a33677642c
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-12-05 13:02:36 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
+    static void printerPos(int x,int y,int turn){
+        if(turn>2){
+            System.exit(0);
+        }
+        if(x>0){
+            System.out.print(x+" ");
+            printerPos(x-5,y,turn);
+        }
+        else{
+            printerNeg(x,y,turn+1);
+        }
+    }
+    static void printerNeg(int x,int y,int turn){
+        if(turn>2){
+            System.exit(0);
+        }
+        if(x<=y){
+            System.out.print(x+" ");
+            printerNeg(x+5,y,turn);
+        }
+        else{
+            printerPos(x,y,turn+1);
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Sayıyı giriniz: ");
+        int inp=scan.nextInt();
+        int n=inp;
+        int turn=1;
+        if(inp>0){
+            printerPos(inp,n,turn);
+        }
+        else{
+            printerNeg(inp,n,turn);
+        }
+
+
+
     }
 }
